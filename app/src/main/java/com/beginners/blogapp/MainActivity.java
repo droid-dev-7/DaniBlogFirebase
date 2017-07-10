06138dde85a02b14d7c632997aa8b24c9d319346
droid-dev-7 <jorgedominguezpoblete@gmail.com>
2017-07-10 19:09:30 +0200

    actu 1

--- app/src/main/java/com/beginners/blogapp/MainActivity.java
+++ app/src/main/java/com/beginners/blogapp/MainActivity.java
@@ -2,13 +2,11 @@ package com.beginners.blogapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
